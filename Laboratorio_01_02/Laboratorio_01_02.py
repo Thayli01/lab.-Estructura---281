045802ffaf5b5ec944ed99be01e3a670523bd368
Thayli01 <156700495+Thayli01@users.noreply.github.com>
2024-03-09 01:07:57 -0500

    Update Laboratorio_01_02.py

--- Laboratorio_01_02/Laboratorio_01_02.py
+++ Laboratorio_01_02/Laboratorio_01_02.py
@@ -3,177 +3,215 @@ from typing import List
 
 # --------------------------------------------------------------------------------
 # 1. Realiza la suma, resta, multiplicación y división de dos números ingresados por el usuario
-# num1 = int(input("a:"))
-# num2 = int(input("b:"))
-# print(f"a + b:{num1+num2}\na - b:{num1-num2}\na xb:{num1*num2}\na/b:{num1/num2}")
+num1 = int(input("a:")) #solicitamos al usuario que ingrese dos numeros a y b
+num2 = int(input("b:"))
+print(f"a + b:{num1+num2}\na - b:{num1-num2}\na x b:{num1*num2}\na / b:{num1/num2}")#imprimimos la suma, resta, multiplicacion y division de a y b
 
 # --------------------------------------------------------------------------------
 # 2. Solicita un número al usuario y determina si es par o impar.
-# numero = int(input("numero:"))
-
-# if numero % 2 == 0:
-#     print(f"{numero} es par")
-# else:
-#     print(f"{numero} es impar")
+numero = int(input("numero:")) #solicitamos al usuario que ingrese un numero
+if numero % 2 == 0: #verificamos: si el residuo al dividir el numero entre 2 es 0, el nuimero es par
+    print(f"{numero} es par")
+else:
+    print(f"{numero} es impar")# si es difereente de 0 es impar
 
 # --------------------------------------------------------------------------------
 # 3. Pide la base y la altura de un triángulo al usuario y calcula su área.
-# base = float(input("base:"))
-# altura = float(input("altura:"))
-# area = base*altura
 
-# print(f"area del triangulo: {area}")
+base = float(input("base:")) #solicitamos al ususario que ingrese la base y altutra de un triangulo
+altura = float(input("altura:"))
+area = base*altura # incilalizamos la variable area con el producto de base por altura
+print(f"area del triangulo: {area}") # imprimimos el area del triangulo
 
 # --------------------------------------------------------------------------------
 # 4. Crea una función que calcule la factorial de un número.
-def factorial (n):
-    if n == 0:
+
+def factorial (n):#definimos la funcion factorial que recibe como parametro n
+    if n == 0:  ## verificamos si n es igual a 0, si es así, la funcion devuelve 1 (ya que el factorial de 0 es 1)
         return 1
     else:
-        return n*factorial (n-1)
-# print(factorial(7))
+        return n*factorial (n-1) # Si n no es igual a 0, devuelve n multiplicado por el factorial de n-1(por ejemplo si n
+                                #   es 7 en la primera iteracion retorna 7 * factorial(6), en la segunda 7*6*factorial(5)...)
+print(factorial(7))
 
 # --------------------------------------------------------------------------------
 # 5. Verifica si un número ingresado por el usuario es primo o no.
+# Definición de una función llamada es_primo que verifica si un número n es primo
 def es_primo(n):
-    es_primo = True
-    for i in range(2, n):
+   
+    es_primo = True # Inicializamos una variable es_primo como True
+    
+    for i in range(2, n):# con for iteramos sobre todos los números desde 2 hasta n-1
+        # con if verificamos si n es divisible por i
         if n % i == 0:
+            # Si n es divisible por i, entonces n no es primo
+            # Se establece la variable es_primo como False y se sale del bucle
             es_primo = False
             break
+    # devuelve True si es_primo sigue siendo True, es decir, si n no es divisible por ningún número entre 2 y n-1
     return es_primo
 
-# n = int(input("Ingrese un número: "))
-# es_primo = es_primo(n)
+# Solicitamos al usuario que ingrese un número
+n = int(input("Ingrese un número: "))
 
-# if es_primo:
-#     print(n, "es primo.")
-# else:
-#     print(n, "no es primo.")
+# Llamamos a la función es_primo para verificar si el número ingresado es primo
+es_primo = es_primo(n)
+
+# se imprime el resultado dependiendo de si el número es primo o no
+if es_primo:
+   print(n, "es primo.")
+else:
+    print(n, "no es primo.")
 
 # --------------------------------------------------------------------------------
 # 6. Toma una cadena de texto y muestra su inversión.
-def invertir_cadena(cadena):
-     return cadena[::-1]
 
-cadena = "Hola, mundo!"
-invertida = invertir_cadena(cadena)
+def invertir_cadena(cadena):# Definimos una función llamada invertir_cadena 
+    # Utiliza la técnica de rebanado de cadenas para invertir la cadena y la devuelve
+    return cadena[::-1]
 
-# print("La cadena invertida es:", invertida)
+
+cadena = "Buenos dias!"# Cadena de texto inicial
+
+# Llamamos a la función invertir_cadena para invertir la cadena y guarda el resultado en la variable cadena_invertida
+cadena_invertida = invertir_cadena(cadena)
+
+# Imprimimos la cadena original y la cadena invertida
+print("La cadena original es:", cadena)
+print("La cadena invertida es:", cadena_invertida)
 
 # --------------------------------------------------------------------------------
 # 7. Calcula la suma de los números pares en un rango especificado por el usuario.
-def sum_peers(start, end):
-    sum: int = 0
-    for i in range(start, end + 1):
-        if i % 2 == 0:
-            sum += i
-    return sum
 
-def sum_range_peers():
-    initial: int = int(input("Enter the start of the range: "))
-    end: int = int(input("Enter the end of the range: "))
-    return sum_peers(initial, end)
+def suma_pares(inicio, final):# se define la funcion suma_pares
+    suma = 0                   #se iniclaiza la variable suma, a la cual se sumaras los pares que se encuentran dentro del rango que ingreso el usuario 
+    for i in range(inicio, final + 1): # i toma los valores que se encuentran dentro del rango
+        if i % 2 == 0: #si el residuo de dividir i entre 2 es cero es un numero par
+            suma += i   #a la variable suma se le agrega i
+    return suma         #la funcion retorna la suma de pares
 
-# print(sum_range_peers())
+def devolver_resutado():  #se define devolver resultado  solicita al usuario ingresar el inicio y el final del rango y devuelve la suma de los números pares dentro de ese rango 
+    inicio = int(input("ingrese el inicio del rango: "))
+    final = int(input("Ingrese el inicio del rango: "))
+    return suma_pares(inicio, final)
+
+print(suma_pares())
 
 # --------------------------------------------------------------------------------
 # 8. Crea una lista de los cuadrados de los primeros 10 números naturales
-def array_sqrt(num):
-    arr_sqrt: List[int] = []
-    for i in range(1, num + 1):
-        arr_sqrt.append(i * i)
+def arreglo_raiz_cuadrada(numero):
+    arreglo_cuadrados: List[int] = []  # Se inicializa una lista vacía para almacenar los cuadrados
+    for i in range(1, numero + 1):  # Itera desde 1 hasta numero + 1
+        arreglo_cuadrados.append(i * i)  # Agrega el cuadrado del número actual a la lista
 
-    return arr_sqrt
+    return arreglo_cuadrados  # Devuelve la lista de cuadrados
 
-# print(array_sqrt(10))
+print(arreglo_raiz_cuadrada(10))  # Imprime la lista de cuadrados de los números del 1 al 10
 
 # --------------------------------------------------------------------------------
 # 9. Cuenta el número de vocales en una cadena de texto.
-def count_vowels(text):
-    count: int = 0
-    for i in text:
-        if i in "aeiouAEIOUáéíóú":
-            count += 1
-    return count
 
-# print(count_vowels("This is a new text"))
+def contador_vocales(texto):
+    contador = 0  # Se inicializa el contador de vocales
+    for caracter in texto:  # Se itera sobre cada caracter en el texto
+        if caracter in "aeiouAEIOUáéíóúÁÉÍÓÚ":  # con se verifica si el caracter es una vocal
+            contador += 1  # si es una vocal el contador incrementa en 1
+    return contador  # la funcion devuelve el total de vocales encontradas
 
+print(contador_vocales("Un texto es una composición de signos codificados en un sistema")) 
 # --------------------------------------------------------------------------------
 # 10. Genera los primeros 10 números de la serie Fibonacci.
-def fibonacci(num):
-    if num <= 1:
-        return num
+
+def fibonacci(numero): #Función recursiva para calcular el número de Fibonacci para un número dado
+    
+    if numero <= 1:  # Caso base: cuando el número es 0 o 1, devuelve el número mismo
+        return numero
     else:
-        return fibonacci(num - 1) + fibonacci(num - 2)
+        return fibonacci(numero - 1) + fibonacci(numero - 2)  # Llamada recursiva para calcular el número de Fibonacci
 
-def array_fibonacci(num):
-    array: List[int] = []
-    for i in range(num):
-        array.append(fibonacci(i))
-    return array
+def arreglo_fibonacci(numero):# la funcion Genera un arreglo de números de fibonacci hasta el número especificado.
+    arreglo = []  # se inicializa un arreglo vacío para almacenar la serie de los números de Fibonacci
+    for i in range(numero):  # se itera desde 0 hasta el número especificado
+        arreglo.append(fibonacci(i))  # se agrega al arreglo el número de fibonacci en la posición actual 
+    return arreglo  # la funcion retorna el arreglo de números de fibonacci
 
-# print(array_fibonacci(10))
-
-
+print(arreglo_fibonacci(10))  # se imprime un arreglo de los 10 primeros números de la serie Fibonacci
 # --------------------------------------------------------------------------------
+
 # 11. Ordena una lista de números ingresados por el usuario de menor a mayor.
-def arr_numbers_sort():
-    arr_numbers: List[int] = []
-    for i in range(int(input("How many numbers do you want to enter? "))):
-        arr_numbers.append(int(input(f"Number {i + 1}: ")))
+def arreglo_ordenado_numeros():
+    arreglo_numeros = []  # Inicializa una lista vacía para almacenar los números
+    for i in range(int(input("¿Cuántos numeros quieres ingresar? "))): # se solicita al usuario la cantidad de números que desea ingresar y recorre ese rango
+        # Solicita al usuario que ingrese cada número y lo agrega a la lista
+        arreglo_numeros.append(int(input(f"Numero {i + 1}: ")))
 
-    arr_numbers.sort()
+    arreglo_numeros.sort()  # se ordena la lista de números de forma ascendente
 
-    return arr_numbers
+    return arreglo_numeros  # Devuelve la lista de números ordenados
 
-# print(arr_numbers_sort())
+print(arreglo_ordenado_numeros())  # se imprime la lista de números ordenados
 
 # --------------------------------------------------------------------------------
 # 12. Verifica si una palabra ingresada por el usuario es un palíndromo.
-def palindromo(text: str):
-    long: int = len(text)
-     half: int = long // 2
+def palindromo(palabra: str):
+    longitud: int = len(palabra)  # se obtiene la longitud de la palabra
+    mitad: int = longitud // 2  # se calcula la mitad de la longitud de la palabra
+    # Iteramos sobre la primera mitad de la palabra
+    for i in range(0, mitad):
+        # se compara los caracteres de la primera mitad con sus correspondientes en la segunda mitad
+        if palabra[i] != palabra[longitud - i - 1]:
+            return False  # Si hay alguna diferencia, la funcion retorna falso, no es palindromo
+    return True  # Si no hay diferencias, retorna true, es palindromo
+# se solicita al usuario ingresar una palabra
+palabra: str = input("Ingrese una palabra: ")
+# Llamada a la función palindromo() y se le pasa como paarmetro la palabra ingresada por el usuario para verificar si la palabra es un palíndromo
+palabra_palindromo = palindromo(palabra)
 
-    for i in range(0, half):
-         if text[i] != text[long - i - 1]:
-             return False
-
-     return True
-
-# text: str = input("input the text: ")
-# text_palindromo = palindromo(text)
-
-# if text_palindromo:
-#     print(text, "is palindromo.")
-# else:
-#     print(text, "not is palindromo.")
+if palabra_palindromo:# Imprime el resultado dependiendo de si la palabra es un palíndromo o no
+    print(palabra, "es un palíndromo.")
+else:
+    print(palabra, "no es un palíndromo.")
 
 # --------------------------------------------------------------------------------
 # 13. Crea un programa que genere la tabla de multiplicar de un número ingresado por el usuario
-def multiplication_table():
-    number: int = int(input("Enter a number to generate your multiplication table: "))
+def tabla_multiplicacion():
+    # Definición de la función que generara la tabla de multiplicar
+
+    numero: int = int(input("Ingresa un número para generar tu tabla de multiplicar: "))
+    # se solicita al usuario que ingrese un número y lo convierte a un entero
+
     for i in range(1, 11):
-        result: int = number * i
-        print(f"{number} x {i} = {result}")
+        # Inicia un bucle que va desde 1 hasta 10
+        resultado: int = numero * i # Calcula el resultado de la multiplicación entre el número ingresado y el multiplicador actual
+        print(f"{numero} x {i} = {resultado}")
+        # Imprime la multiplicación en formato "número x multiplicador = resultado"
 
-# multiplication_table()
+tabla_multiplicacion()# se llama a la función para generar la tabla de multiplicar
 
 # --------------------------------------------------------------------------------
+
 # 14. Pide el radio de un círculo al usuario y calcula su área.
-def area_circunference():
-    radius: int = int(input("Enter the radius of the circle: "))
-    area: int = math.pi * pow(radius, 2)
-    return f"The area of the circumference: {area:.6}"
 
-# print(area_circunference())
+def area_circunferencia(): # función para calcular el área de la circunferencia
+
+    radio = float(input("Ingrese el radio de la circunferencia: ")) # se solicita al usuario que ingrese el radio de la circunferencia y lo almacena como un número flotante
+
+    area = math.pi * pow(radio, 2) # Calculamos el área de la circunferencia utilizando la fórmula π * radio^2
+    return f"Área de la circunferencia: {area}"
+    # Devuelve el resultado del área de la circunferencia 
+print(area_circunferencia()) # Imprime el resultado del cálculo del área de la circunferencia
 
 # --------------------------------------------------------------------------------
-# 15. Toma un número entero y calcula la suma de sus dígitos.
-def sum_digit(num: int):
-    sum: int = 0
-    for i in str(num):
-        sum = sum + int(i)
-    return sum
 
-print(sum_digit(565))
+# 15. Toma un número entero y calcula la suma de sus dígitos.
+def suma_digitos(numero: int):# función para calcular la suma de los dígitos de un número entero
+    suma: int = 0 # Inicializa la variable suma para almacenar la suma de los dígitos
+    for i in numero:
+        # Iteramos sobre cada dígito del número 
+        suma = suma + i
+        # cada dígito se suma a la variable suma
+    return suma
+    # la funcion retorna el resultado de la suma de los dígitos
+
+print(suma_digitos(565))
+# Llamamos a la función suma_digitos() con el número 565 y no imprime 16
